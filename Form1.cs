75ea0ef8677654dbd6d7394ad786ba714d0d924c
Rafael Teixeira <fael.teixeira@gmail.com>
2019-09-01 03:55:58 -0300

    Calculadora Básica feita

---
+++ Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculos_basicos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnSomar_Click(object sender, EventArgs e)
+        {
+            // Declaração de variáveis do tipo double, num1, num2, resultado.
+            double num1, num2, resultado;
+            /*num1 e num2 recebe conteúdo do textbox (txtNum1.text e txtNum2.text)
+            Convert.Todouble é necessário para que seja convertido
+            o conteúdo do textbox para número.*/
+            num1 = Convert.ToDouble(txtNum1.Text);
+            num2 = Convert.ToDouble(txtNum2.Text);
+            //Variável resultado = recebe conteúdo de num1 + num2 e realiza o cálculo.
+            resultado = num1 + num2;
+            // txtResultado.Text = recebe conteúdo da variável resultado e converte para número.
+            txtResultado.Text = resultado.ToString();
+        }
+
+        private void BtnMultiplicar_Click(object sender, EventArgs e)
+        {
+            double num1, num2, resultado;
+            num1 = Convert.ToDouble(txtNum1.Text);
+            num2 = Convert.ToDouble(txtNum2.Text);
+            resultado = num1 * num2;
+            txtResultado.Text = resultado.ToString();
+        }
+
+        private void BtnSubtrair_Click(object sender, EventArgs e)
+        {
+            double num1, num2, resultado;
+            num1 = Convert.ToDouble(txtNum1.Text);
+            num2 = Convert.ToDouble(txtNum2.Text);
+            resultado = num1 - num2;
+            txtResultado.Text = resultado.ToString();
+        }
+
+        private void BtnDividir_Click(object sender, EventArgs e)
+        {
+            double num1, num2, resultado;
+            num1 = Convert.ToDouble(txtNum1.Text);
+            num2 = Convert.ToDouble(txtNum2.Text);
+            resultado = num1 / num2;
+            txtResultado.Text = resultado.ToString();
+        }
+
+        private void BtnLimpar_Click(object sender, EventArgs e)
+        {
+            txtNum1.Text = "";
+            txtNum2.Text = "";
+            txtResultado.Text = "";
+        }
+
+        private void BtnSair_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Calculadora Básica
+
+Uma calculadora básica que foi desenvolvida durante o curso Linguagem de Programação C# - Básico, da Fundação Bradesco.
